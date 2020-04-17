434ddbd980177a0ad99b53bfc9f039094d98a37d
Jo <jo-lion@hotmail.fr>
2020-04-17 18:51:06 +0200

    refacto dossiers

---
+++ Front/JS/index.js
@@ -0,0 +1,34 @@
+//Affichage des oursons
+fetch("http://localhost:3000/api/teddies")
+    .then(response => response.json()
+        .then(data => {
+
+            if (response.ok) {
+                console.log(data);
+
+                for (let i = 0; i < data.length; i++) {
+                    let nomOurson = data[i].name;
+                    let imgOurson = data[i].imageUrl;
+
+                    let imgOursons = document.createElement("img");
+                    imgOursons.src = data[i].imageUrl;
+
+                    let liOursons = document.createElement("li");
+                    let aOursons = document.createElement("a");
+                    aOursons.href = data[i]._id;
+
+                    let pOursons = document.createElement('p');
+                    pOursons.textContent = nomOurson;
+
+                    let divOursons = document.getElementById("produits");
+                    divOursons.appendChild(liOursons);
+                    liOursons.appendChild(imgOursons);
+                    aOursons.appendChild(pOursons);
+                    liOursons.appendChild(aOursons);
+                }
+
+            } else {
+                console.error("retour du serveur : ", response.status);
+            }
+        }))
+
