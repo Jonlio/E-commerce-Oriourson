2d5bd9d2784b5ae2dd127b6ce7337a1bfe16f415
Jo <jo-lion@hotmail.fr>
2020-04-21 15:57:15 +0200

    refact produits

--- Front/JS/index.js
+++ Front/JS/index.js
@@ -1,21 +1,20 @@
 //Affichage des Oursons
 async function affichTeddies() {
     let response = await fetch('http://localhost:3000/api/teddies');
-    let data = await response.json();
+    let datas = await response.json();
 
     if (response.ok) {
-        console.log(data);
+        console.log(datas);
 
-        for (let i = 0; i < data.length; i++) {
-            let nomOurson = data[i].name;
-            let imgOurson = data[i].imageUrl;
+        for (let i = 0; i < datas.length; i++) {
+            let nomOurson = datas[i].name;
+            let imgOurson = datas[i].imageUrl;
             let imgOursons = document.createElement("img");
-            imgOursons.src = data[i].imageUrl;
-
+            imgOursons.src = datas[i].imageUrl;
 
             let liOursons = document.createElement("li");
             let aOursons = document.createElement("a");
-            aOursons.href = 'produits.html?id=' + data[i]._id;
+            aOursons.href = 'produits.html?id=' + datas[i]._id;
 
             let pOursons = document.createElement('p');
             pOursons.textContent = nomOurson;
--- Front/JS/produits.js
+++ Front/JS/produits.js
@@ -1,21 +1,22 @@
 // Affichage Ourson Selectionne
-let section = document.querySelector('#produit');
-let params = new URLSearchParams(window.location.search); //URL query parameters
+let params = new URLSearchParams(window.location.search); 
 let id = params.get("id");
-let api = 'http://localhost:3000/api/teddies/';
-
 let request = new XMLHttpRequest();
-request.open('GET', api + id);
+request.open('GET', 'http://localhost:3000/api/teddies/' + id);
 request.responseType = 'json';
 request.send();
 
 //Fonctions dynamiques
 request.onload = function () {
-    let product = request.response;
-    console.log(product);
+    let data = request.response;
     let {oursChoisi, oursImage, oursNom, oursDescri, oursPrix} = varElements();
-    elementsContent(product, oursImage, oursNom, oursDescri, oursPrix);
+    elementsContent(data, oursImage, oursNom, oursDescri, oursPrix);
     appendiceChild(oursChoisi, oursImage, oursNom, oursDescri, oursPrix);
+};
+
+//Error
+request.onerror = function () {
+    alert('Server error, please try again later');
 };
 
 //Fonctions détaillees
@@ -26,26 +27,23 @@ function varElements() {
     let oursNom = document.createElement('h3');
     let oursDescri = document.createElement('p');
     let oursPrix = document.createElement('p');
-    return {oursChoisi, oursImage, oursNom, oursDescri, oursPrix,};
+    return {oursChoisi, oursImage, oursNom, oursDescri, oursPrix};
 }
 
 
-function elementsContent(product, oursImage, oursNom, oursDescri, oursPrix) {
-    oursImage.src = product.imageUrl;
-    oursNom.textContent = product.name;
-    oursDescri.textContent = product.description;
-    oursPrix.textContent = 'Prix: ' + product.price / 100 + '€';
+function elementsContent(data, oursImage, oursNom, oursDescri, oursPrix) {
+    oursImage.src = data.imageUrl;
+    oursNom.textContent = data.name;
+    oursDescri.textContent = data.description;
+    oursPrix.textContent = 'Prix: ' + data.price / 100 + '€';
 }
 
 function appendiceChild(oursChoisi, oursImage, oursNom, oursDescri, oursPrix) {
-    produit.appendChild(oursChoisi);
+    let section = document.querySelector('#produit');
+    section.appendChild(oursChoisi);
     oursChoisi.appendChild(oursImage);
     oursChoisi.appendChild(oursNom);
     oursChoisi.appendChild(oursDescri);
     oursChoisi.appendChild(oursPrix);
 }
 
-//Else
-request.onerror = function () {
-    alert('Server error, please try again later');
-};
