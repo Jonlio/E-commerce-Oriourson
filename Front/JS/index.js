87446098811f0c365f736081796d29535dce448a
Jo <jo-lion@hotmail.fr>
2020-04-22 14:06:28 +0200

    refacto affichTeddy()

--- Front/JS/index.js
+++ Front/JS/index.js
@@ -1,26 +1,26 @@
 //Affichage des Oursons
 async function affichTeddies() {
     let response = await fetch('http://localhost:3000/api/teddies');
-    let data = await response.json();
+    let datas = await response.json();
 
     if (response.ok) {
-        console.log(data);
+        console.log(datas);
 
-        for (let i = 0; i < data.length; i++) {
-            let nomOurson = data[i].name;
-            let imgOurson = data[i].imageUrl;
-            let imgOursons = document.createElement("img");
-            imgOursons.src = data[i].imageUrl;
+        for (let i = 0; i < datas.length; i++) {
+            let nomOurson = datas[i].name;
+            let imgOurson = datas[i].imageUrl;
 
             let liOursons = document.createElement("li");
+            let imgOursons = document.createElement("img");
             let aOursons = document.createElement("a");
-            aOursons.href = 'produits.html?id=' + data[i]._id;
-
             let h3Oursons = document.createElement('h3');
+
+            imgOursons.src = datas[i].imageUrl;
+            aOursons.href = 'produits.html?id=' + datas[i]._id;
             h3Oursons.textContent = nomOurson;
 
-            let divOursons = document.getElementById("produits");
-            divOursons.appendChild(liOursons);
+            let ulOursons = document.getElementById("produits");
+            ulOursons.appendChild(liOursons);
             liOursons.appendChild(imgOursons);
             aOursons.appendChild(h3Oursons);
             liOursons.appendChild(aOursons);
--- Front/JS/produits.js
+++ Front/JS/produits.js
@@ -1,58 +1,39 @@
 // Affichage Ourson Selectionne
-let params = new URLSearchParams(window.location.search);
-let id = params.get("id");
-let request = new XMLHttpRequest();
-request.open('GET', 'http://localhost:3000/api/teddies/' + id);
-request.responseType = 'json';
-request.send();
+function affichTeddy() {
+    let params = new URLSearchParams(window.location.search);
+    let id = params.get("id");
+    let request = new XMLHttpRequest();
+    request.open('GET', 'http://localhost:3000/api/teddies/' + id);
+    request.responseType = 'json';
+    request.send();
 
-//Fonctions dynamiques
-request.onload = function () {
-    let data = request.response;
-    let {
-        oursChoisi,
-        oursImage,
-        oursNom,
-        oursDescri,
-        oursPrix
-    } = displOurson();
-    caractOurson(data, oursImage, oursNom, oursDescri, oursPrix);
-    appendiceChild(oursChoisi, oursImage, oursNom, oursDescri, oursPrix);
-};
+    request.onload = function () {
+        let data = request.response;
+        console.log(data);
 
-//Error
-request.onerror = function () {
-    alert('Server error, please try again later');
-};
+        let oursChoisi = document.createElement('article');
+        let oursImage = document.createElement('img');
+        let oursNom = document.createElement('h3');
+        let oursDescri = document.createElement('p');
+        let oursPrix = document.createElement('p');
 
-//Fonctions détaillees
-function displOurson() {
-    let oursChoisi = document.createElement('article');
-    let oursImage = document.createElement('img');
-    let oursNom = document.createElement('h3');
-    let oursDescri = document.createElement('p');
-    let oursPrix = document.createElement('p');
-    return {
-        oursChoisi,
-        oursImage,
-        oursNom,
-        oursDescri,
-        oursPrix
-    };
+        oursImage.src = data.imageUrl;
+        oursNom.textContent = data.name;
+        oursDescri.textContent = data.description;
+        oursPrix.textContent = 'Prix: ' + data.price / 100 + '€';
+
+        let section = document.querySelector('#produit');
+        section.appendChild(oursChoisi);
+        oursChoisi.appendChild(oursImage);
+        oursChoisi.appendChild(oursNom);
+        oursChoisi.appendChild(oursDescri);
+        oursChoisi.appendChild(oursPrix);
+    }
+
+    //Error
+    request.onerror = function () {
+        alert('Server error, please try again later');
+    }
 }
 
-function caractOurson(data, oursImage, oursNom, oursDescri, oursPrix) {
-    oursImage.src = data.imageUrl;
-    oursNom.textContent = data.name;
-    oursDescri.textContent = data.description;
-    oursPrix.textContent = 'Prix: ' + data.price / 100 + '€';
-}
-
-function appendiceChild(oursChoisi, oursImage, oursNom, oursDescri, oursPrix) {
-    let section = document.querySelector('#produit');
-    section.appendChild(oursChoisi);
-    oursChoisi.appendChild(oursImage);
-    oursChoisi.appendChild(oursNom);
-    oursChoisi.appendChild(oursDescri);
-    oursChoisi.appendChild(oursPrix);
-}
+affichTeddy()
