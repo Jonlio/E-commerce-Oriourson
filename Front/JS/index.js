cea11439a141e5c12a2c4c627b48a6edb1d7e487
Jo <jo-lion@hotmail.fr>
2020-04-24 00:59:12 +0200

    fonction recup

--- Front/JS/index.js
+++ Front/JS/index.js
@@ -1,31 +1,28 @@
-//Affichage des Oursons
-async function affichOursons() {
-    let response = await fetch('http://localhost:3000/api/teddies');
-    let datas = await response.json();
+// Recupère tous les Oursons
+const recupOursons = async () => {
+    const response = await fetch('http://localhost:3000/api/teddies');
+    return await response.json();
+}
 
-    if (response.ok) {
-        console.log(datas);
+// Affiche tous les Oursons
+const affichOursons = async () => {
+    const datas = await recupOursons()
+    for (let i = 0; i < datas.length; i++) {
 
-        for (let i = 0; i < datas.length; i++) {
-            
-            let liOursons = document.createElement("li");
-            let imgOursons = document.createElement("img");
-            let aOursons = document.createElement("a");
-            let h3Oursons = document.createElement('h3');
+        let liOursons = document.createElement("li");
+        let imgOursons = document.createElement("img");
+        let aOursons = document.createElement("a");
+        let nomOursons = document.createElement('h3');
 
-            imgOursons.src = datas[i].imageUrl;
-            aOursons.href = 'produits.html?id=' + datas[i]._id;
-            h3Oursons.textContent = datas[i].name;
+        imgOursons.src = datas[i].imageUrl;
+        aOursons.href = 'produits.html?id=' + datas[i]._id;
+        nomOursons.textContent = datas[i].name;
 
-            let ulOursons = document.getElementById("produits");
-            ulOursons.appendChild(liOursons);
-            liOursons.appendChild(imgOursons);
-            aOursons.appendChild(h3Oursons);
-            liOursons.appendChild(aOursons);
-        }
-
-    } else {
-        console.error("retour du serveur : ", response.status);
+        let ulOursons = document.getElementById("produits");
+        ulOursons.appendChild(liOursons);
+        liOursons.appendChild(imgOursons);
+        aOursons.appendChild(nomOursons);
+        liOursons.appendChild(aOursons);
     }
 }
 
