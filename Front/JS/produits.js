2e22e331423f35dc0f29b891a061e7fdd97d2779
Jo <jo-lion@hotmail.fr>
2020-04-27 10:14:40 +0200

    refacto fonctions + ajout pannier

--- Front/JS/produits.js
+++ Front/JS/produits.js
@@ -1,15 +1,14 @@
 // Affichage Ourson Selectionne
-function affichOurson() {
+async function recupOurson() {
     let params = new URLSearchParams(window.location.search);
     let id = params.get("id");
-    let request = new XMLHttpRequest();
-    request.open('GET', 'http://localhost:3000/api/teddies/' + id);
-    request.responseType = 'json';
-    request.send();
+    response = await fetch("http://localhost:3000/api/teddies/" + id);
+    let data = await response.json();
+    return data;
+}
 
-    request.onload = function () {
-        let data = request.response;
-        let colors = data.colors;
+recupOurson()
+    .then(function (data) {
 
         let oursChoisi = document.createElement('article');
         let oursImage = document.createElement('img');
@@ -17,11 +16,15 @@ function affichOurson() {
         let oursDescri = document.createElement('p');
         let oursPrix = document.createElement('p');
         let oursListColor = document.createElement('select');
+        let lienPannier = document.createElement('a');
+        let bouton = document.createElement('p');
 
         oursImage.src = data.imageUrl;
         oursNom.textContent = data.name;
         oursDescri.textContent = data.description;
         oursPrix.textContent = 'Prix: ' + data.price / 100 + '€';
+        lienPannier.href = 'pannier.html'
+        bouton.textContent = 'Je l\'adopte!';
 
         let section = document.querySelector('#produit');
         section.appendChild(oursChoisi);
@@ -30,18 +33,15 @@ function affichOurson() {
         oursChoisi.appendChild(oursDescri);
         oursChoisi.appendChild(oursPrix);
         oursChoisi.appendChild(oursListColor);
-        
+        oursChoisi.appendChild(lienPannier);
+        lienPannier.appendChild(bouton);
+
+        let colors = data.colors;
         for (let color of colors) {
             let choixCouleur = document.createElement('option');
             choixCouleur.textContent = color;
             oursListColor.appendChild(choixCouleur);
-            }
-    }
+        }
+    })
 
-    //Error
-    request.onerror = function () {
-        console.log('Server error, please try again later');
-    }
-}
 
-affichOurson()
