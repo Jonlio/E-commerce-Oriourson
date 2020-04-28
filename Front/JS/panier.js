09e02d00cf79cba68314b6e1f4f6eaf4df9f238f
Jo <jo-lion@hotmail.fr>
2020-04-28 09:42:08 +0200

    panier/prix total

--- Front/JS/panier.js
+++ Front/JS/panier.js
@@ -1,20 +1,27 @@
-//Panier
+//Récupération localStorage
 let panier = JSON.parse(localStorage.getItem('panier'));
-console.log(panier)
 
+//Affichage contenu panier
 for (let i = 0; i < panier.length; i++) {
     let oursChoisi = document.createElement('article');
-    let oursImage = document.createElement('img');
     let oursNom = document.createElement('h3');
     let oursPrix = document.createElement('p');
 
-    oursImage.src = panier[i].image;
     oursNom.textContent = panier[i].name;
     oursPrix.textContent = 'Prix: ' + panier[i].price / 100 + '€';
 
     let section = document.querySelector('#section');
     section.appendChild(oursChoisi);
-    oursChoisi.appendChild(oursImage);
     oursChoisi.appendChild(oursNom);
     oursChoisi.appendChild(oursPrix);
 }
+
+//Affichage prix total
+let total = 0;
+for (let i = 0; i < panier.length; i++) {
+    total += panier[i].price;
+}
+
+let prixTotal = document.createElement('p');
+prixTotal.textContent = 'Le montant total de votre panier est de ' + total / 100 + ' €';
+section.appendChild(prixTotal);
